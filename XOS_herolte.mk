0e08ed5d23a5ad6cc47163592d598ecb77d906ad
Yuv <dailytroll92@gmail.com>
2017-09-02 23:08:47 -0600

    Silly mistakes hehe

--- XOS_herolte.mk
+++ XOS_herolte.mk
@@ -1,4 +1,4 @@
-# Inherit device configuration 
+# Inherit device configuration
 $(call inherit-product, device/samsung/herolte/herolte.mk)
 $(call inherit-product, device/samsung/hero-common/hero-common.mk)
 
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AOSP stuff.
- $(call inherit-product, vendor/xos/common.mk)
+ $(call inherit-product, vendor/xos/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := XOS_herolte
